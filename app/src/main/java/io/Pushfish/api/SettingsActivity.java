40967998b8f5e6b8230b3bcb96ed8b5e2242cfec
Víctor A. Rodríguez (Bit-Man) <victor@bit-man.guru>
2018-10-31 13:52:36 -0300

    Replace http://api.pushjet.io with https://api.push.fish

--- app/src/main/java/io/Pushfish/api/SettingsActivity.java
+++ app/src/main/java/io/Pushfish/api/SettingsActivity.java
@@ -29,7 +29,7 @@ public class SettingsActivity extends PreferenceActivity {
      * shown on tablets.
      */
     private static final boolean ALWAYS_SIMPLE_PREFS = false;
-    private static final String DEFAULT_PUSHFISH_GCM_REGISTER_URL = "https://api.pushjet.io";
+    private static final String DEFAULT_PUSHFISH_GCM_REGISTER_URL = "https://api.push.fish";
     private static final String DEFAULT_SENDER_ID = "509878466986";
 
     private static Preference.OnPreferenceClickListener sBindOnPreferenceClickListener = new Preference.OnPreferenceClickListener() {
