b493f1423edf7144cb35dbeccb7e1184efba2b7f
Jason G. Villanueva <a@jsonvillanueva.com>
2019-04-10 02:26:45 -0700

    Finished PushFish main screen. 1/7.

--- app/src/main/java/fish/push/api/pushfish/PushFishActivity.java
+++ app/src/main/java/fish/push/api/pushfish/PushFishActivity.java
@@ -1,6 +1,8 @@
 package fish.push.api.pushfish;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,8 +14,32 @@ public class PushFishActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pushfish);
-        Toolbar myToolbar = findViewById(R.id.toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose"Settings"
+                return true;
+
+            case R.id.action_servers:
+                // User chose "Servers"
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
